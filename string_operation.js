a991096f5245213e24844247ffef0d3855908aac
Raghunandan Yadav <90599332+Raghu-N-Yadav@users.noreply.github.com>
2021-09-13 15:10:57 +0530

    operations like '+, -), Calculating lenght

---
+++ string_operation.js
@@ -0,0 +1,8 @@
+console.log('Hello' + 'World');
+console.log('Hello' + ' ' + 'World');
+//console.log('Hello'); .. This line is not valid
+
+console.log('Hi' + ',' + 'How' + ',' + 'are' + ',' + 'you');
+
+//give length of string
+console.log('Raghu'.length);
