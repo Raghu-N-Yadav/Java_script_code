1c2f6c99423132d94f6fc3578af66a8acdef9782
Raghunandan Yadav <90599332+Raghu-N-Yadav@users.noreply.github.com>
2021-09-13 15:31:00 +0530

    some string methods

---
+++ string_methods.js
@@ -0,0 +1,13 @@
+// doing some string methods 
+
+console.log('hello'.toUpperCase()); // give HELLO
+console.log('RAGHU'.toLowerCase()); // Give raghu
+console.log('Super'.startsWith('h')); // false .. check first letter
+console.log('Happy'.startsWith('H')); // True
+
+let str = '     happy go lucky      ' // defining variable as a string
+console.log(str);
+console.log('lenght is : ', str.length); //gives length
+console.log(str.trim()); // remove white space
+console.log(str.trimStart()); //remove white space from star
+console.log(str.trimEnd()); //remove white space from end
