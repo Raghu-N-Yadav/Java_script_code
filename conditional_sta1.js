dbb4c5cbf900aa7c7b1f39b7ef02e06ee9f3230f
Raghunandan Yadav <90599332+Raghu-N-Yadav@users.noreply.github.com>
2021-09-13 20:25:38 +0530

    nested if else and switch statement

---
+++ conditional_sta1.js
@@ -0,0 +1,10 @@
+//using nested if else statements
+let fruit = 'apple';
+
+if (fruit === 'apple'){
+    console.log(`Your favorite fruite is ${fruit}`);
+} else if (fruit === 'mango'){
+    console.log(`Your favorite fruit is ${fruit}`);
+} else {
+    console.log('You don\'t like fruits');
+}
---
+++ conditional_switch.js
@@ -0,0 +1,23 @@
+//using random number genrator to play with switch
+let myNumber = Math.floor(Math.random()* 5);
+
+switch (myNumber) {
+    case 1:
+        console.log(`Number is ${myNumber}`);
+        break;
+    case 2:
+        console.log(`Number is ${myNumber}`);
+        break;
+    case 3:
+        console.log(`The  number is ${myNumber}`);
+        break;
+    case 4:
+        console.log(`This time number is ${myNumber}`);
+        break;
+    case 5:
+        console.log(`This is the number ${myNumber}`);
+        break;
+    default:
+        console.log('I think there is a ZERO');
+        break;
+}
