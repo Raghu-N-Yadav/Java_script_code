673de0c75470e4251322612872233f9741594e87
Raghunandan Yadav <90599332+Raghu-N-Yadav@users.noreply.github.com>
2021-09-13 15:55:54 +0530

    using in built object math

---
+++ math_object.js
@@ -0,0 +1,8 @@
+//using abulit in object .. like math
+
+console.log(Math.random()); // random number b/w 0 nad 1
+var number = Math.random() * 50; // random number b/w 0 and 50 and store in number
+console.log(number);
+console.log(Math.floor(number)); //  gives nearest floor value 
+console.log(Math.round(25.66)); // round to nearest integer
+console.log(Math.pow(2,3));// give 2^3
