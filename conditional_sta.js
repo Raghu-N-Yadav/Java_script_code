0e569750703917b251360458f34337d2fbbf5d81
Raghunandan Yadav <90599332+Raghu-N-Yadav@users.noreply.github.com>
2021-09-13 19:55:41 +0530

    using if else & ternary operators

---
+++ conditional_sta.js
@@ -0,0 +1,16 @@
+// using conditional stements like if ..
+// () ? : ; -- ternary operation like if else 
+let workingLevel = 'hard';
+ if (workingLevel === 'hard'){
+     console.log('You will get a job.');
+ }else {
+     console.log('Cahnce are less Dear !!');
+ }
+
+ //reassinging the value of variable 
+ workingLevel = 'medium';
+
+ //using ternary operation
+ (workingLevel === 'hard') ? console.log('You will definatly get a job.') : console.log('Cahnces are less.. try harder');
+
+ 
