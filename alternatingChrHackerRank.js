571167695bee4bdcbd03de3b19b48e740902b5bd
Raghunandan Yadav <90599332+Raghu-N-Yadav@users.noreply.github.com>
2021-10-28 15:46:49 +0530

    check alternating string

---
+++ alternatingChrHackerRank.js
@@ -0,0 +1,68 @@
+/*
+You are given a string containing characters  and  only. 
+Your task is to change it into a string such that there are no matching adjacent characters. 
+To do this, you are allowed to delete zero or more characters in the string.
+
+Your task is to find the minimum number of required deletions.
+*/
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'alternatingCharacters' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+function alternatingCharacters(s) {
+    let del =0;
+    let i =0;
+    while (i < s.length-1){
+        if (s[i] == s[i+1]){
+            del ++
+        }
+        i++
+    }
+    return del
+    // Write your code here
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine().trim(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const s = readLine();
+
+        const result = alternatingCharacters(s);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
